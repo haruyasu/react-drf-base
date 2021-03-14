4e54b4ced95feb0d42bfd886cac4bf6a3b37975c
haruyasu <harukun2002@gmail.com>
2021-03-14 20:52:06 +0900

    コンポーネント作成

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # react-drf-base
 
 
-cd frontend
-npx create-react-app . --template redux-typescript --use-npm
-npm install axios
-npm install @reduxjs/toolkit
-npm install tailwindcss
-npm install -D tailwindcss@npm:@tailwindcss/postcss7-compat @tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9
+cd frontend  
+npx create-react-app . --template redux-typescript --use-npm  
+npm install axios  
+npm install @reduxjs/toolkit  
+npm install tailwindcss  
+npm install -D tailwindcss@npm:@tailwindcss/postcss7-compat @tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9  
+npm install react-router-dom @types/react-router-dom  
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './components/Home/Home'
+import Login from './components/Auth/Login'
+import Signup from './components/Auth/Signup'
+import ResetPassword from './components/Auth/ResetPassword'
+import ResetPasswordConfirm from './components/Auth/ResetPasswordConfirm'
+import Activate from './components/Auth/Activate'
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div>
-      test
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/signup' component={Signup} />
+        <Route exact path='/reset-password' component={ResetPassword} />
+        <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
+        <Route exact path='/activate/:uid/:token' component={Activate} />
+      </Switch>
+    </BrowserRouter>
   )
 }
 
-export default App;
+export default App
